786f692cb8f272ac1f06ee3f7c56f830f20fedfb
abhishek201202 <abhishekk201202@gmail.com>
2024-09-01 13:53:53 +0530

    added snake and ladder lld

---
+++ Low Level Design/Interview Questions/23. Snake and Ladder/main.cpp
@@ -0,0 +1,52 @@
+/*
+
+FR - 
+1. Can have multiple dice / snakes / ladder / player
+2. game will finished when a player wins
+3. Snakes / Ladder are auto generated OR given by user ?
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+------------------------
+Player 
+------------------------
+int id;
+Position currPos;
+
+------------------------
+Dice
+------------------------
+int diceCount;
+int rollDice();
+
+------------------------
+Jump (snake, ladder)
+------------------------
+int start;
+int end;
+
+
+------------------------
+Board
+------------------------
+vector<vector<Cell>> board
+
+
+------------------------
+Cell
+------------------------
+int cellId;
+int jumpId;
+
+
+------------------------
+Game
+------------------------
+Board boardObj;
+Dice diceObj;
+Deque<Player> players;
+
+*/
