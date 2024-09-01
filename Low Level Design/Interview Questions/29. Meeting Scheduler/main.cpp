a84c1440e9cfdfd55a3ddc0bf022b63059e2a2c0
abhishek201202 <abhishekk201202@gmail.com>
2024-09-01 16:37:48 +0530

    added meeting scheduler lld

---
+++ Low Level Design/Interview Questions/29. Meeting Scheduler/main.cpp
@@ -0,0 +1,9 @@
+/*
+meeting scheduler - https://www.youtube.com/watch?v=MRx40JVmmF4
+
+FR - 
+1. there are n meeting rooms
+2. Book a meeting in any meeting room at given interval (start time and end time, capacity)
+3. send notification to all the persons who are invited to meeting.
+4. use meeting room calendar to track the meetings date and time (current day).
+*/
