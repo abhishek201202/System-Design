378ab0691e6239635386bbbace8f0408048021de
abhishek201202 <abhishekk201202@gmail.com>
2024-09-03 15:09:09 +0530

    fix

--- Low Level Design/Design pattern/3. Behavioral Design Pattern/4. Iterater/main.cpp
+++ Low Level Design/Design pattern/3. Behavioral Design Pattern/4. Iterater/main.cpp
@@ -14,11 +14,11 @@ public:
 // concrete iterator
 class ArrayIterator : public Iterator {
 private:
-    const int* data;
+    const vector<int> data;
     int size;
     int index;
 public:
-    ArrayIterator(const int* data, int size) : data(data), size(size), index(0) {}
+    ArrayIterator(const vector<int> data, int size) : data(data), size(size), index(0) {}
     bool hasNext() const override {
         return index < size;
     }
@@ -36,23 +36,31 @@ public:
 // concrete aggregater 
 class ArrayAggregate : public Aggregate {
 private:
-    int* data;
+    vector<int> data;
     int size;
 public:
-    ArrayAggregate(int* data, int size) : data(data), size(size) {}
+    ArrayAggregate(vector<int> data, int size) : data(data), size(size) {}
     Iterator* createIterator() const override {
         return new ArrayIterator(data, size);
     }
-    ~ArrayAggregate() {
-        delete[] data;
+    ~ArrayAggregate() {}
+};
+
+class AggregateFactory {
+public:
+    static Aggregate* getAggregator(auto &data, int size) {
+        if(typeid(data).name() == "St6vectorIxSaIxEE") {
+          return new ArrayAggregate(data, size);
+        }
+        return nullptr;
     }
 };
 
 // client
 int main() {
-    int* data = new int[5]{1, 2, 3, 4, 5};
-    ArrayAggregate aggregate(data, 5);
-    Iterator* iterator = aggregate.createIterator();
+    vector<int> data = {1, 2, 3, 4, 5};
+    Aggregate* aggregate = AggregateFactory::getAggregator(data, 5);
+    Iterator* iterator = aggregate -> createIterator();
     while (iterator->hasNext()) {
         cout << iterator->next() << " ";
     }
