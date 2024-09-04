a76a07934d812c0b9b482763b956d6b13043943c
abhishek201202 <abhishekk201202@gmail.com>
2024-09-04 23:01:08 +0530

    fix

--- Low Level Design/Interview Questions/30. Zomato/main.cpp
+++ Low Level Design/Interview Questions/30. Zomato/main.cpp
@@ -1,9 +1,11 @@
 
 /*
+Video - https://www.youtube.com/watch?v=Zo_YfDmkZSQ
+
 Functional Requirements:
 1. User login
 2. Search restaurent based on location.
-3.List all the food items for a particular restaurent.
+3. List all the food items for a particular restaurent.
 4. Placing a particular order.
 5. making payment
 6. giving a notification to user.
