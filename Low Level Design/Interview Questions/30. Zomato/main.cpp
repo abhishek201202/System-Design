89b002457956b60c8cf7f4195405a160d55bbc03
abhishek201202 <abhishekk201202@gmail.com>
2024-09-04 10:42:40 +0530

    added zomato lld

---
+++ Low Level Design/Interview Questions/30. Zomato/main.cpp
@@ -0,0 +1,38 @@
+
+/*
+Functional Requirements:
+1. User login
+2. Search restaurent based on location.
+3.List all the food items for a particular restaurent.
+4. Placing a particular order.
+5. making payment
+6. giving a notification to user.
+7. Live tracking of food.
+
+Non Functional Requirements:
+1. Scalable
+2. modular / maintainble / reliable
+
+Actors:
+1. Restaurent
+2. Customers
+3. Delivery Partners
+
+Main Entity:
+1. User
+2. Customer
+3. Restaurent
+4. FoodItems
+5. Cart
+6. Payment
+7. Orders
+8. Notification System
+
+
+Class Diagram:
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
