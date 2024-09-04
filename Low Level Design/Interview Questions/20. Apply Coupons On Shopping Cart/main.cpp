ac2c16237cca7f6999b24d32283214ad42c60ce8
abhishek201202 <abhishekk201202@gmail.com>
2024-09-04 19:29:00 +0530

    fix

--- Low Level Design/Interview Questions/20. Apply Coupons On Shopping Cart/main.cpp
+++ Low Level Design/Interview Questions/20. Apply Coupons On Shopping Cart/main.cpp
@@ -16,8 +16,8 @@ private:
     double price;
 public:
     Product(const string& name, double price) : name(name), price(price) {}
-    double getPrice() const return price;
-    string getName() const return name;
+    double getPrice() const { return price; }
+    string getName() const { return name; }
 };
 
 class Cart {
