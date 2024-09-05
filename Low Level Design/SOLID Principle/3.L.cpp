ebb1298e8ac0e06d0a5a674da279c9518e62b683
abhishek201202 <abhishekk201202@gmail.com>
2024-09-05 12:20:10 +0530

    added L of SOLID

--- Low Level Design/SOLID Principle/3.L.cpp
+++ Low Level Design/SOLID Principle/3.L.cpp
@@ -6,30 +6,62 @@ object A with B without breaking the behaviour of program
 #include<bits/stdc++.h>
 using namespace std;
 
-class Bird {
+// Violating Liskov substitution principle
+class Rectangle {
 public:
-    virtual void fly() {
-        std::cout << "Flying in the sky." << std::endl;
+    virtual void setWidth(double w) { width = w; }
+    virtual void setHeight(double h) { height = h; }
+    double getArea() const { return width * height; }
+
+protected:
+    double width = 0, height = 0;
+};
+
+class Square : public Rectangle {
+public:
+    void setWidth(double w) override {
+        width = w;
+        height = w; // Violation: Square is not substitutable for Rectangle
+    }
+    
+    void setHeight(double h) override {
+        width = h;
+        height = h; // Violation: Square is not substitutable for Rectangle
     }
 };
 
-class Penguin : public Bird {
+// Following Liskov substitution principle
+class Shape {
 public:
-    void fly() override {
-        throw std::logic_error("Penguins can't fly!");
-    }
+    virtual double getArea() const = 0; // pure virtual function
+    virtual ~Shape() = default; // virtual destructor for polymorphism
 };
 
-void makeBirdFly(Bird& bird) {
-    bird.fly();
-}
+class Rectangle : public Shape {
+public:
+    Rectangle(double w, double h) : width(w), height(h) {}
+    
+    void setWidth(double w) { width = w; }
+    void setHeight(double h) { height = h; }
 
-int main() {
-    Bird sparrow;
-    Penguin penguin;
+    double getArea() const override {
+        return width * height;
+    }
 
-    makeBirdFly(sparrow);  // Works fine
-    makeBirdFly(penguin);  // Throws an error at runtime
+protected:
+    double width, height;
+};
 
-    return 0;
-}
+class Square : public Shape {
+public:
+    Square(double side) : sideLength(side) {}
+
+    void setSideLength(double side) { sideLength = side; }
+
+    double getArea() const override {
+        return sideLength * sideLength;
+    }
+
+private:
+    double sideLength;
+};
